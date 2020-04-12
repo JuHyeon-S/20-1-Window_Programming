9823953818940639d4c156367bdf09746f7fa8f7
JuHyeon-S <seo99832@daum.net>
2020-04-13 04:04:22 +0900

    3주차 연습문제

---
+++ week03/p01/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace p01
+{
+    class Program
+    {
+        static void WriteLog(string format, params object[] arg)
+        {
+            var s = string.Format(format, arg);
+            Console.WriteLine(s);
+        }
+        static public void Main()
+        {
+            string user = "C#";
+            string message = "가변길이 매개변수 활용";
+            WriteLog("Time = {0} User = {1}  Message = {2}", 3, user, message);
+        }
+
+    }
+}
---
+++ week03/p02/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace p02
+{
+    class Person
+    {
+        public int Number { get; set; }
+        public string Name { get; set; }
+        public string Information => $"name = {Name}  number = {Number} ";
+    }
+    class Program
+    {
+        static public void Main(string[] args)
+        {
+            Person person = new Person()
+            {
+                //초기화 하고자 하는 프로퍼티만 선택가능, 세미콜론이 아닌 콤머로 구분
+                Number = 2020,
+                Name = "C#"
+            };
+            Console.WriteLine(person.Information);
+        }
+    }
+
+}
