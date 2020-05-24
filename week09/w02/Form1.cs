b8663ca787ca0cd56b4c8927d417db59250b0190
JuHyeon-S <seo99832@daum.net>
2020-05-25 08:32:49 +0900

    w01/w02

---
+++ week09/w02/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace w02
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void rChange(RadioButton r)
+        {
+            if (r.Checked)
+            {
+                string s = r.Text;
+                switch (s)
+                {
+                    case "왼쪽":
+                        label3.TextAlign = ContentAlignment.MiddleLeft;
+                        break;
+                    case "가운데":
+                        label3.TextAlign = ContentAlignment.MiddleCenter;
+                        break;
+                    case "오른쪽":
+                        label3.TextAlign = ContentAlignment.MiddleRight;
+                        break;
+                }
+            }
+        }
+        private void cChange(CheckBox c)
+        {
+            string s = c.Text;
+            if (c.Checked)
+            {
+                switch(s)
+                {
+                    case "굵게":
+                        label3.Font = new Font(label3.Font, label3.Font.Style | FontStyle.Bold);
+                        break;
+                    case "밑줄":
+                        label3.Font = new Font(label3.Font, label3.Font.Style | FontStyle.Underline);
+                        break;
+                    case "이탤릭":
+                        label3.Font = new Font(label3.Font, label3.Font.Style | FontStyle.Italic);
+                        break;
+                    case "취소선":
+                        label3.Font = new Font(label3.Font, label3.Font.Style | FontStyle.Strikeout);
+                        break;
+                }
+            }
+            else
+            {
+                switch (s)
+                {
+                    case "굵게":
+                        label3.Font = new Font(label3.Font, label3.Font.Style ^ FontStyle.Bold);
+                        break;
+                    case "밑줄":
+                        label3.Font = new Font(label3.Font, label3.Font.Style ^ FontStyle.Underline);
+                        break;
+                    case "이탤릭":
+                        label3.Font = new Font(label3.Font, label3.Font.Style ^ FontStyle.Italic);
+                        break;
+                    case "취소선":
+                        label3.Font = new Font(label3.Font, label3.Font.Style ^ FontStyle.Strikeout);
+                        break;
+                }
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Color[] colors = { Color.DarkOrange, Color.Blue, Color.Brown, Color.DarkGreen, Color.Cyan, Color.DarkBlue, Color.Purple, Color.BlueViolet };
+            Random r = new Random();
+            int i = r.Next() % colors.Length;
+            label3.BackColor = colors[i];
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            RadioButton r = sender as RadioButton;
+            rChange(r);
+        }
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            CheckBox c = sender as CheckBox;
+            cChange(c);
+        }
+    }
+}
---
+++ week09/w03/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace w03
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(83, 107);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(379, 42);
+            this.textBox1.TabIndex = 0;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 30;
+            this.listBox1.Location = new System.Drawing.Point(83, 199);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(378, 364);
+            this.listBox1.TabIndex = 1;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(721, 199);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(322, 38);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(505, 108);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(280, 41);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "콤보상자에 추가";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(505, 287);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(167, 55);
+            this.button2.TabIndex = 4;
+            this.button2.Text = ">>";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(505, 414);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(167, 55);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "<<";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.CheckOnClick = true;
+            this.checkedListBox1.ColumnWidth = 300;
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.HorizontalScrollbar = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "영화감상",
+            "음악감상",
+            "스키",
+            "스노우보드",
+            "수영",
+            "농구",
+            "장기",
+            "골프",
+            "바둑",
+            "축구"});
+            this.checkedListBox1.Location = new System.Drawing.Point(83, 622);
+            this.checkedListBox1.MultiColumn = true;
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(589, 277);
+            this.checkedListBox1.TabIndex = 6;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(721, 622);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(261, 59);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "리스트에 추가";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(18F, 30F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1082, 969);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "리스트상자 & 콤보상자 & 체크리스트상자";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
